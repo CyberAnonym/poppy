c7e05477a8c0db719d0c1cfc32756da84f9e7fd3
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-26 14:28:42 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -66,8 +66,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
-
+language = 'en_US'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
