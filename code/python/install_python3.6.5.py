5afc4dfd9160c6542e279afbf06cf178c5733731
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-27 16:52:01 +0800

    update

--- code/python/install_python3.6.5.py
+++ code/python/install_python3.6.5.py
@@ -6,7 +6,7 @@ if os.path.exists('/usr/bin/python3'):
     print('python3 is exist already.')
     exit(1)
 
-subprocess.call('yum install gcc zlib zlib-devel libffi-devel -y',shell=True)
+subprocess.call('yum install gcc zlib zlib-devel libffi-devel openssl-devel -y',shell=True)
 os.chdir('/tmp')
 if os.path.exists('/tmp/python3.6.5.tar.xz'):
     pass
@@ -19,6 +19,8 @@ subprocess.call('./configure --prefix=/usr/local/python3',shell=True)
 subprocess.call('make',shell=True)
 subprocess.call('make install',shell=True)
 subprocess.call("sed -i '210,212s/#//' /usr/local/src/Python-3.6.5/Modules/Setup",shell=True)
+subprocess.call("sed -i '205s/#//' /usr/local/src/Python-3.6.5/Modules/Setup",shell=True)
+
 subprocess.call('make',shell=True)
 subprocess.call('make install',shell=True)
 subprocess.call('ln -s /usr/local/python3/bin/python3 /usr/bin/',shell=True)
