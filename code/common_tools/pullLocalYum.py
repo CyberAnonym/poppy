7aa24fa9cfbd2c256b568c4ac851e1e242527a32
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-08-27 11:45:11 +0800

    alvin update

--- code/common_tools/pullLocalYum.py
+++ code/common_tools/pullLocalYum.py
@@ -14,10 +14,10 @@ def backupOriginRepo():
             shutil.move(os.path.join(originDIR,file),os.path.join(backDIR,file))
 def getRepo():
     systemVersion=re.findall(r'\d',os.popen('cat /etc/redhat-release').read())[0]
-    repoUrl='https://raw.githubusercontent.com/AlvinWanCN/tech-center/master/software/yum.repos.d/centos%s.dc.alv.pub.repo'%systemVersion
+    repoUrl='https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/yum.repos.d/centos%s.dc.alv.pub.repo'%systemVersion
     os.system('curl -fsSL %s > /etc/yum.repos.d/centos%s.dc.alv.pub.repo'%(repoUrl,systemVersion))
-    os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/tech-center/master/software/yum.repos.d/epel.dc.alv.pub.repo > /etc/yum.repos.d/epel.dc.alv.pub.repo')
-    os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/tech-center/master/software/yum.repos.d/sophiroth.repo > /etc/yum.repos.d/sophiroth.repo')
+    os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/yum.repos.d/epel.dc.alv.pub.repo > /etc/yum.repos.d/epel.dc.alv.pub.repo')
+    os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/yum.repos.d/sophiroth.repo > /etc/yum.repos.d/sophiroth.repo')
     os.system('yum repolist')
 def main():
     backupOriginRepo()
