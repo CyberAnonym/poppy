d3eef0d9f3b5b6f4e216996e86a703cb8f566018
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-11-09 20:40:45 +0800

    alvin update

--- code/production/jdk_tomcat_mysql.sh
+++ code/production/jdk_tomcat_mysql.sh
@@ -2,20 +2,25 @@
 
 
 #定义提示格式
-
-notes(){
+notes()
+{
     echo -e "\033[034m$1 \033[0m"
 }
 
-#定义提示格式
-
-echo0(){ #定义绿色字体输出success 红色字体是31m.绿色字是32m# 黄色颜色是33m,
+#定义提示格式 #定义绿色字体输出success 红色字体是31m.绿色字是32m# 黄色颜色是33m
+echo0()
+{
 echo -e "$1 \033[032m [success] \033[0m"
 }
-echo1(){ #定义红色字体输出failed
+
+echo1()
+{ #定义红色字体输出failed
 echo -e "$1 \033[031m [Failed] \033[0m"
 }
-checkLastCommand(){ #定义确认命令是否执行成功的消息输出的函数
+
+#定义确认命令是否执行成功的消息输出的函数
+checkLastCommand()
+{
 if [ $? -eq 0 ];then
     echo0 "$1"
     else
@@ -26,7 +31,7 @@ fi
 
 
 #创建安装java的函数
-function install_java()
+install_java()
 {
     notes '现在开始解压jdk-8u191-linux-x64.tar.gz '
     tar xf jdk-8u191-linux-x64.tar.gz -C /usr/local/
@@ -35,21 +40,26 @@ function install_java()
     echo 'export JAVA_HOME=/usr/local/jdk1.8.0_191' >> /etc/profile
     echo 'export PATH=$JAVA_HOME/bin:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$PATH' >> /etc/profile
     checkLastCommand 'JAVA环境变量已添加至/etc/profile' '环境变量添加失败'
+    #重新引用环境变量
     source /etc/profile
     notes '以下是当前系统JAVA版本信息'
     java -version
 }
 
 #创建安装tomcat的函数
-function install_tomcat()
+install_tomcat()
 {
     notes '现在开始解压tomcat 到/usr/local/'
     tar xf apache-tomcat-8.5.34.tar.gz -C /usr/local/
     checkLastCommand '解压apache-tomcat-8.5.34.tar.gz到/usr/local/成功' '解压apache-tomcat-8.5.34.tar.gz失败'
+    notes '现在开始往/usr/local/apache-tomcat-8.5.34/bin/catalina.sh里写入JAVA_HOME和JRE_HOME的值'
+    sed -i '1a export JAVA_HOME=/usr/local/jdk1.8.0_191/' /usr/local/apache-tomcat-8.5.34/bin/catalina.sh
+    sed -i '2a export JRE_HOME=/usr/local/jdk1.8.0_191/jre/' /usr/local/apache-tomcat-8.5.34/bin/catalina.sh
+    checkLastCommand '环境变量写入完成' '失败'
 }
 
 #创建安装mysql的函数
-function install_mysql()
+install_mysql()
 {
     notes '现在开始解压mysql-server_5.7.24-1ubuntu16.04_amd64.deb-bundle.tar'
     tar xf mysql-server_5.7.24-1ubuntu16.04_amd64.deb-bundle.tar
@@ -64,7 +74,6 @@ function install_mysql()
     notes '现在开始安装libmecab2_0.996-1.2ubuntu1_amd64.deb'
     dpkg -i libmecab2_0.996-1.2ubuntu1_amd64.deb
     checkLastCommand 'libmecab2_0.996-1.2ubuntu1_amd64.deb 安装完成' '安装失败'
-    #dpkg -i libm*
     #wget http://ftp.br.debian.org/debian/pool/main/liba/libaio/libaio1_0.3.110-3_amd64.deb
     notes '现在开始安装libaio1_0.3.110-3_amd64.deb'
     dpkg -i libaio1_0.3.110-3_amd64.deb
@@ -84,25 +93,18 @@ function install_mysql()
     notes '现在开始安装mysql-community-server_5.7.24-1ubuntu16.04_amd64.deb'
     #开始安装
     dpkg -i mysql-community-server_5.7.24-1ubuntu16.04_amd64.deb
-
     checkLastCommand 'mysql-community-server_5.7.24-1ubuntu16.04_amd64.deb安装完成' '安装失败'
 }
 
 #创建设置mysql的函数
-function setup_mysql()
+setup_mysql()
 {
     notes '正在设置数据库密码'
-
     mysql -uroot -proot -e 'grant all privileges on *.* to "root"@"localhost" identified by "root"'
-
     checkLastCommand '密码设置完成' '失败'
-
     notes '现在开始创建数据库'
-
     mysql -uroot -proot -e 'create database yufeng_cj;'
-
     checkLastCommand '数据库创建完成' '失败'
-
     notes '现在开始执行sql初始化库'
     mysql -uroot -proot yufeng_cj < yufeng_cj.sql
     checkLastCommand '数据库初始化完毕' '失败'
@@ -110,11 +112,15 @@ function setup_mysql()
 
 #创建主函数
 main (){
-
+    #执行安装java的函数
     install_java
+    #执行安装tomcat的函数
     install_tomcat
+    #执行安装mysql的安装
     install_mysql
+    #执行进行mysql相关操作的函数
     setup_mysql
+    #开始进行总结通知
     notes '所有操作已完成。'
     notes 'tomcat路径:  /usr/local/apache-tomcat-8.5.34/'
     notes '启动tomcat命令： /usr/local/apache-tomcat-8.5.34/bin/startup.sh'
