9fb9f5907cba149eab5930535882b0cf726b61a8
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-11-12 16:32:04 +0800

    alvin update

--- code/common_tools/create_local_repo.py
+++ code/common_tools/create_local_repo.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import subprocess
 
+#创建用于挂载镜像的目录
 subprocess.call('mkdir -p /mnt/iso',shell=True)
-
-subprocess.call()
+#设置将进行自动挂载到/mnt/iso目录
+subprocess.call('grep /mnt/iso /etc/fstab || echo "/dev/sr0 /mnt/iso   iso9660 defaults,loop 0 0" >> /etc/fstab"',shell=True)
+#让/etc/fstab里的挂载设置生效
+subprocess.call('mount -a')
+#从网上下载base.repo配置文件到本地
+subprocess.call('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/yum.repos.d/base.repo > /etc/yum.repos.d/base.repo',shell=True)
+#查看yum仓库的信息
+subprocess.call('yum repolist',shell=True)
