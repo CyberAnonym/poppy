b03293d6af4cda25276bc2a53918f1c90d8863f1
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-12-19 00:17:10 +0800

    alvin update

--- code/alv.pub/auto_deploy_ntp_client.py
+++ code/alv.pub/auto_deploy_ntp_client.py
@@ -3,14 +3,14 @@
 import subprocess
 
 #本脚本网络地址：https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/auto_deploy_ntp_client.py
-#shell下一键使用： curl -s https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/auto_deploy_nfs_client.py|python
+#shell下一键使用： curl -s https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/auto_deploy_ntp_client.py|python
 
 appdict={}
-#appdict['ntp_server']='ipa.alv.pub'
+appdict['ntp_server']='ipa.alv.pub'
 appdict['src_file']='https://raw.githubusercontent.com/AlvinWanCN/poppy/master/conf.d/common/etc/chrony.conf'
 appdict['dest_file']='/etc/chrony.conf'
 appdict['app_name']='chrony'
-
+appdict['svc_name']='chronyd'
 
 ##Install chrony
 def install_chrony():
@@ -22,7 +22,7 @@ def install_chrony():
 
 #setup configuration file
 def set_cfg_file():
-    if subprocess.call('curl -s {src_file} > {dst_file}'.format(**appdict)) == 0:
+    if subprocess.call('curl -s {src_file} > {dest_file} && sed -i "s/ipa.alv.pub/{ntp_server}/" {dest_file}'.format(**appdict),shell=True) == 0:
         print('configuration file write done')
     else:
         print('configuration file write failed, exit process')
@@ -30,7 +30,7 @@ def set_cfg_file():
 
 #startup app
 def startup_app():
-    if subprocess.call('systemctl start {app_name}'.format(**appdict),shell=True):
+    if subprocess.call('systemctl restart {svc_name}'.format(**appdict),shell=True) == 0:
         print('{app_name} started successfully'.format(**appdict))
     else:
         print('{app_name} started failed'.format(**appdict))
@@ -41,7 +41,7 @@ def main():
     install_chrony()
     set_cfg_file()
     startup_app()
-    del appdict
 
 if __name__ == '__main__':
     main()
+    del appdict
