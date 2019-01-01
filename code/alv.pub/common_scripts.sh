833d83fbdfaeaf9a7ab42878151b69eef3eda673
Alvin Wan <alvin.wan.cn@hotmail.com>
2019-01-01 23:39:37 +0800

    alvin update

--- code/alv.pub/common_scripts.sh
+++ code/alv.pub/common_scripts.sh
@@ -2,8 +2,8 @@
 
 curl -s https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/set_hostinfo_by_nic.py|python  #设置网络信息
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/common_tools/sshslowly.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/common_tools/sshslowly.sh)"  #优化ssh
 
-python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/common_tools/pullLocalYum.py)"
+python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/common_tools/pullLocalYum.py)" #拉取本地镜像
 
-curl -s https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/setup_zabbix_agent.py|python #安装zabbix客户端
+python -c "$(curl -s https://raw.githubusercontent.com/AlvinWanCN/poppy/master/code/alv.pub/setup_zabbix_agent.py)" #安装zabbix客户端
